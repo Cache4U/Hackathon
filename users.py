0477dd10ba39128428acafb5cb91f20e23afcc03
bfady <bfady33@gmail.com>
2021-03-30 16:08:33 +0300

    check parameters and added requests

--- users.py
+++ users.py
@@ -5,16 +5,27 @@ class Unit:
     def __init__(self, unit_id, prob = 0):
         self.unit_id = unit_id
         self.prob = prob
-        self.past_queries = []
+        self.past_requests = []
         self.anafs = []
 
     def add_anaf(self, anaf_id, anaf_prob):
+        for anaf in self.anafs:
+            if anaf.anaf_id == anaf_id:
+                return None
         new_anaf = Anaf(self.unit_id, anaf_id, anaf_prob)
         self.anafs.append(new_anaf)
         return new_anaf
 
-    def generate_query(self,):
-        pass
+    def generate_requests(self, anaf_id, mador_id):
+        if random.random() < self.prob:
+            #todo return fresher requests
+            return random.choice(self.past_requests)
+
+        else:
+            for anaf in self.anafs:
+                if anaf.anaf_id == anaf_id:
+                    return anaf.generate_requests(mador_id)
+
 
 
 class Anaf:
@@ -26,12 +37,22 @@ class Anaf:
         self.madors = []
 
     def add_mador(self, mador_id, mador_prob):
+        for mador in self.madors:
+            if mador.mador_id == mador_id:
+                return None
         new_mador = Mador(self.unit, self.anaf_id, mador_id, mador_prob)
         self.madors.append(new_mador)
         return new_mador
 
-    def generate_request(self):
-        pass
+    def generate_request(self, mador_id):
+        if random.random() < self.prob:
+            #todo return fresher requests
+            return random.choice(self.past_requests)
+
+        else:
+            for mador in self.madors:
+                if mador.mador_id == mador_id:
+                    return mador.generate_requests()
 
 
 class Mador:
@@ -44,12 +65,15 @@ class Mador:
         self.users = []
 
     def add_user(self, user_id):
+        for user in self.users:
+            if user.user_id == user_id:
+                return None
         new_user = User(self.unit, self.anaf, self.mador_id, user_id)
         self.users.append(new_user)
         return new_user
 
     def generate_request(self):
-        pass
+        return random.choice(self.past_requests)
 
 
 class User:
@@ -62,10 +86,10 @@ class User:
         self.prob = prob
 
     def generate_request(self, unit):
-        if random.random() < self.prob:
+        if random.random() < self.prob or len(unit.past_requrests) != 0:
             return Request(self.unit, self.anaf, self.mador, self.user_id)
         else:
-            pass
+            return unit.generate_requests(self.anaf, self.mador)
 
     def update_request_rate(self, rq):
         self.request_rate = rq
@@ -77,10 +101,9 @@ class DataItem:
 
 
 class Request:
-    def __init__(self, unit_id, anaf_id, mador_id, user_id):
+    def __init__(self, unit_id, anaf_id, mador_id, user_id, queue):
         self.unit_id = unit_id
         self.anaf_id = anaf_id
         self.mador_id = mador_id
         self.user_id = user_id
-
-
+        self.queue = queue
